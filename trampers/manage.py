e2b7b9e7dc90e1a9bfdeb2680ce7a64482473c09
Kalesh <kaleshcv2@gmail.com>
2020-04-28 23:53:11 +0530

    business

--- trampers/manage.py
+++ trampers/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travello.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trampers.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
